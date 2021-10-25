88507487ecb69f08cc219e2265f08b8ccab65c97
Giadaspt <saputo.giada@gmail.com>
2021-10-25 15:41:46 +0200

    codice js semplificato

--- js/script.js
+++ js/script.js
@@ -5,30 +5,23 @@ const contain = document.getElementById('contain');
 for ( let i = 1; i <= 100; i++) {
   const box = document.createElement('box');
   box.className = 'box';
-  box.innerHTML = i ;
+  box.innerHTML = i;
   contain.append(box);
 
   if (i % 3 == 0){
     box.classList.add('fizz');
-    let fizz = document.querySelector('.fizz');
-    fizz.innerHTML = 'fizz';
+    box.innerText = 'fizz';
     console.log('fizz',i);
   }
   if (i % 5 == 0){
     box.classList.add('buzz');
-    let buzz = document.querySelector('.buzz');
-    buzz.innerHTML = 'buzz';
+    box.innerText = 'buzz';
     console.log('buzz', i);
   }
   if ((i % 3 == 0) && (i % 5 == 0) ){
     box.classList.add('fizzbuzz');
-    let fizzbuzz = document.querySelector('.fizzbuzz');
-    fizzbuzz.innerHTML = 'fizzbuzz';
+    box.innerText = 'fizzbuzz';
     console.log('fizzbuzz', i);
   }
-
-
-
-
 
 }
