f29e58db968e335cff9a44c7b695d613c243d5ef
Giadaspt <saputo.giada@gmail.com>
2021-10-25 14:37:30 +0200

    multipli di 3 e di 5 e poi entrambi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 
-const container = document.getElementById('contain');
+const contain = document.getElementById('contain');
 
-for ( let i = 0; i < 100; i++) {
+
+for ( let i = 1; i <= 100; i++) {
   const box = document.createElement('box');
   box.className = 'box';
-  box.innerHTML = i + 1;
-  container.append(box);
-  console.log('ciao');
+  box.innerHTML = i ;
+  contain.append(box);
+
+  if (i % 3 == 0){
+    box.classList.add('fizz');
+    let fizz = document.querySelector('.fizz');
+    fizz.innerHTML = 'fizz';
+    console.log('fizz',i);
+  }
+  if (i % 5 == 0){
+    box.classList.add('buzz');
+    let buzz = document.querySelector('.buzz');
+    buzz.innerHTML = 'buzz';
+    console.log('buzz', i);
+  }
+  if ((i % 3 == 0) && (i % 5 == 0) ){
+    box.classList.add('fizzbuzz');
+    let fizzbuzz = document.querySelector('.fizzbuzz');
+    fizzbuzz.innerHTML = 'fizzbuzz';
+    console.log('fizzbuzz', i);
+  }
+
+
+
+
+
 }
