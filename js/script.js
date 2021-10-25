d74c2dcd75417063f438f61a5c885489c1d08322
Giadaspt <saputo.giada@gmail.com>
2021-10-25 17:41:59 +0200

    tolti commenti precedenti

--- js/script.js
+++ js/script.js
@@ -10,17 +10,14 @@ for ( let i = 1; i <= 100; i++) {
 
   if ((i % 3 == 0) && (i % 5 == 0) ){
     box.classList.add('fizzbuzz');
-    //box.innerText = 'fizzbuzz';
     text = 'fizzbuzz';
     console.log('fizzbuzz', i);
   } else if (i % 3 == 0){
     box.classList.add('fizz');
-    //box.innerText = 'fizz';
     text = 'fizz';
     console.log('fizz',i);
   } else if (i % 5 == 0){
     box.classList.add('buzz');
-    //box.innerText = 'buzz';
     text = 'buzz';
     console.log('buzz', i);
   }
