b5804d1b30451a402bf0c314172d5aa7d12bbf6a
Giadaspt <saputo.giada@gmail.com>
2021-10-25 17:41:05 +0200

    codice migliorato

--- js/script.js
+++ js/script.js
@@ -5,23 +5,26 @@ const contain = document.getElementById('contain');
 for ( let i = 1; i <= 100; i++) {
   const box = document.createElement('box');
   box.className = 'box';
-  box.innerHTML = i;
+  let text = i;
   contain.append(box);
 
-  if (i % 3 == 0){
-    box.classList.add('fizz');
-    box.innerText = 'fizz';
-    console.log('fizz',i);
-  }
-  if (i % 5 == 0){
-    box.classList.add('buzz');
-    box.innerText = 'buzz';
-    console.log('buzz', i);
-  }
   if ((i % 3 == 0) && (i % 5 == 0) ){
     box.classList.add('fizzbuzz');
-    box.innerText = 'fizzbuzz';
+    //box.innerText = 'fizzbuzz';
+    text = 'fizzbuzz';
     console.log('fizzbuzz', i);
+  } else if (i % 3 == 0){
+    box.classList.add('fizz');
+    //box.innerText = 'fizz';
+    text = 'fizz';
+    console.log('fizz',i);
+  } else if (i % 5 == 0){
+    box.classList.add('buzz');
+    //box.innerText = 'buzz';
+    text = 'buzz';
+    console.log('buzz', i);
   }
+
+  box.innerHTML = text;
 
 }
